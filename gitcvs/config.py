ced7c1d9e8af15410fcc2934a70d4797ed8fa54e
Michael Johnson <Michael.Johnson@sas.com>
2012-06-26 15:58:49 -0400

    Allow ${ENVVAR} in config file values
    In order to make it reasonable to share configuration files between
    users without creating permissions problems, values in the configuration
    files may now reference environment variables using ${VARIABLENAME}
    syntax.  This means that any literal $ characters must be escaped by
    doubling: $$

--- gitcvs/config.py
+++ gitcvs/config.py
@@ -1,3 +1,5 @@
+import os
+import string
 import ConfigParser
 
 class Config(ConfigParser.SafeConfigParser):
@@ -17,8 +19,23 @@ class Config(ConfigParser.SafeConfigParser):
                     value = self.get(section, option)
                     if not value.startswith('/'):
                         raise ValueError('"[%s] %s = %s":'
-                            ' Value must be absolute path starting with /'
+                            ' Value must resolve to absolute path starting with /'
                             %(section, option, value))
+
+    def get(self, *args, **kwargs):
+        # cannot use the "var" argument because the environment overrides
+        # content in the file.  Using ${} instead of %()s syntax for that
+        # reason.
+        v = ConfigParser.SafeConfigParser.get(self, *args, **kwargs)
+        t = string.Template(v)
+        return t.substitute(os.environ)
+
+    def items(self, *args, **kwargs):
+        # cannot use the "var" argument because it adds a keyword for
+        # each environment variable, so we have to restrict it to the
+        # keywords actually present and then interpolate
+        i = ConfigParser.SafeConfigParser.items(self, *args, **kwargs)
+        return [(x[0], self.get(args[0], x[0])) for x in i]
 
     def openConfig(self, configFileName):
         return open(configFileName)
--- unit_test/config_test.py
+++ unit_test/config_test.py
@@ -8,19 +8,41 @@ from gitcvs import config
 class TestConfig(testutils.TestCase):
     def setUp(self):
         self.fd, self.cf = tempfile.mkstemp(suffix='.gitcvs')
-        file(self.cf, 'r+').write('[foo]')
+        file(self.cf, 'r+').write('[foo]\nbar = ${FAKEHOME}\n')
         self.cfg = config.Config(self.cf)
+        if 'FAKEHOME' in os.environ:
+            self.fakehome = os.environ['FAKEHOME']
+        else:
+            self.fakehome = None
+        os.environ['FAKEHOME'] = '/tmp'
 
     def tearDown(self):
         os.close(self.fd)
         os.remove(self.cf)
+        if self.fakehome:
+            os.environ['FAKEHOME'] = self.fakehome
+        else:
+            os.unsetenv('FAKEHOME')
 
     def test_openConfig(self):
-        self.assertEqual(self.cfg.openConfig(self.cf).read(), '[foo]')
+        self.assertEqual(self.cfg.openConfig(self.cf).read(),
+                         '[foo]\nbar = ${FAKEHOME}\n')
 
     def test_readConfig(self):
         self.cfg.readConfig(os.fdopen(os.dup(self.fd)))
         self.assertEqual(self.cfg.sections(), ['foo'])
+
+    def test_getEnv(self):
+        self.assertEqual(self.cfg.get('foo', 'bar'), '/tmp')
+
+    def test_itemsEnv(self):
+        self.assertEqual(self.cfg.items('foo'), [('bar', '/tmp')])
+
+    def test_requireAbsolutePathsFromEnvironment(self):
+        cfgstr = StringIO('[foo]\nbardir = ${FAKEHOME}/blah\n')
+        cfg = config.Config(cfgstr)
+        # asserts both that there is no ValueError and correct interpolation
+        self.assertEqual(cfg.get('foo', 'bardir'), '/tmp/blah')
 
     def test_requireDirAbsolutePaths(self):
         badcfg = StringIO('[foo]\nbardir = baz\n')
